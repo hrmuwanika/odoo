0d19a104ef0361513bb37d567366df498a1deb7b
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-04-19 20:38:16 +0200

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -48,6 +48,7 @@ ADMIN_EMAIL="odoo@example.com"
 echo -e "\n---- Update Server ----"
 sudo apt update
 sudo apt upgrade -y
+sudo apt autoremove -y
 
 #--------------------------------------------------
 # Install PostgreSQL Server
@@ -63,7 +64,7 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 # Install Dependencies
 #--------------------------------------------------
 echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 gdebi -y
+sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt1-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 gdebi -y
                                                                        
 echo -e "\n---- Install python packages/requirements ----"
 sudo pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
@@ -82,7 +83,8 @@ sudo npm install -g rtlcss
 ## https://www.odoo.com/documentation/13.0/setup/install.html#debian-ubuntu
 
 wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb
-sudo apt install wkhtmltox_0.12.5-1.bionic_amd64.deb
+sudo dpkg -i wkhtmltox_0.12.5-1.bionic_amd64.deb
+sudo apt -f install -y
 
 echo -e "\n---- Create ODOO system user ----"
 sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
