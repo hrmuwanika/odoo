b7d0e6c255a901498bd172d669e4000f4b80bae8
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-04-25 09:20:29 +0200

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -181,7 +181,7 @@ PermissionsStartOnly=true
 SyslogIdentifier=odoo-server
 User=$OE_USER
 Group=$OE_USER
-ExecStart=$OE_HOME_EXT/odoo-bin --config=/etc/${OE_CONFIG}.conf --addons-path=$OE_HOME_EXT/addons/
+ExecStart=$OE_HOME_EXT/odoo-bin --config /etc/${OE_CONFIG}.conf  --logfile /var/log/${OE_USER}/${OE_CONFIG}.log
 WorkingDirectory=$OE_HOME_EXT
 StandardOutput=journal+console
 
