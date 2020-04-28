1238127bf04dc9948389d1cce338a00eb2ea96f0
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-04-29 01:28:27 +0200

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -208,69 +208,71 @@ if [ $INSTALL_NGINX = "True" ]; then
   sudo apt install nginx -y
   sudo systemctl enable nginx
   
-  cat <<EOF > ~/odoo
-  #odoo server
+cat <<EOF > ~/odoo
+#odoo server
 upstream odoo {
-    server 127.0.0.1:$OE_PORT weight=1 fail_timeout=0;
+    server 127.0.0.1:$OE_PORT;
 }
 
 upstream odoochat {
-    server 127.0.0.1:$LONGPOLLING_PORT weight=1 fail_timeout=0;
+    server 127.0.0.1:$LONGPOLLING_PORT;
 }
 
-# http -> https
+# http to https redirection
 server {
-   listen 80;
-   listen [::]:80;
+    listen 80;
+    listen [::]:80;
+    
+    # listen 443 ssl;
+    # listen [::]:443 ssl;
+    server_name $WEBSITE_NAME;
    
-   #listen 443;
-   #listen [::]:443;
-   server_name $WEBSITE_NAME;
-   proxy_read_timeout 720s;
-   proxy_connect_timeout 720s;
-   proxy_send_timeout 720s;
-
-   # Add Headers for odoo proxy mode
-   proxy_set_header X-Forwarded-Host \$host;
-   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-   proxy_set_header X-Forwarded-Proto \$scheme;
-   proxy_set_header X-Real-IP \$remote_addr;
-
-   # SSL parameters
-   # ssl on;
-   # ssl_certificate /etc/letsencrypt/live/$WEBSITE_NAME/fullchain.pem;
-   # ssl_certificate_key /etc/letsencrypt/live/$WEBSITE_NAME/privkey.pem;
-   # ssl_session_timeout 30m;
-   # ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-   # ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
-   # ssl_prefer_server_ciphers on;
-
-   # log
-   access_log /var/log/nginx/$OE_USER-access.log;
-   error_log /var/log/nginx/$OE_USER-error.log;
-
-   # Redirect requests to odoo backend server
-   location / {
-   proxy_redirect off;
-   proxy_pass http://odoo;
-   }
-
-   location /longpolling {
-   proxy_pass http://odoochat;
-   }
-
-   # cache some static data in memory for 60mins.
-   # under heavy load this should relieve stress on the OpenERP web interface a bit.
-   location ~* /[0-9a-zA-Z_]*/static/ {
-                proxy_cache_valid 200 60m;
+    # Proxy settings
+    proxy_read_timeout 720s;
+    proxy_connect_timeout 720s;
+    proxy_send_timeout 720s;
+   
+    # Add Headers for odoo proxy mode
+    proxy_set_header X-Forwarded-Host \$host;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Real-IP \$remote_addr;
+   
+    # SSL parameters
+    # ssl on;
+    # ssl_certificate /etc/letsencrypt/live/$WEBSITE_NAME/fullchain.pem;
+    # ssl_certificate_key /etc/letsencrypt/live/$WEBSITE_NAME/privkey.pem;
+    # ssl_trusted_certificate /etc/letsencrypt/live/$WEBSITE_NAME/chain.pem;
+    # ssl_session_timeout 30m;
+    # ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    # ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
+    # ssl_prefer_server_ciphers on;
+   
+    # log
+    access_log /var/log/nginx/$OE_USER-access.log;
+    error_log /var/log/nginx/$OE_USER-error.log;
+   
+    # Request for root domain
+    location / {
+    proxy_redirect off;
+    proxy_pass http://odoo;
+    }
+   
+    location /longpolling {
+    proxy_pass http://odoochat;
+    }
+   
+    # Cache static files.
+    location ~* /[0-9a-zA-Z_]*/static/ {
+                proxy_cache_valid 200 90m;
                 proxy_buffering on;
                 expires 864000;
                 proxy_pass http://odoo;
-   }
-
-   # common gzip
-   gzip_types text/css text/scss text/plain text/xml application/xml application/json application/javascript;
-   gzip on;
+    }
+   
+    # Gzip Compression
+    gzip_types text/css text/scss text/plain text/xml application/xml application/json application/javascript;
+    gzip on;
 }
 EOF
 
