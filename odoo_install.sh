ab85b55685ac90dc25d54073dd62b9256d8bb36e
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-04-28 20:25:55 +0200

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -226,10 +226,10 @@ server {
    proxy_send_timeout 720s;
 
    # Add Headers for odoo proxy mode
-   proxy_set_header X-Forwarded-Host $host;
-   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-   proxy_set_header X-Forwarded-Proto $scheme;
-   proxy_set_header X-Real-IP $remote_addr;
+   proxy_set_header X-Forwarded-Host \$host;
+   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+   proxy_set_header X-Forwarded-Proto \$scheme;
+   proxy_set_header X-Real-IP \$remote_addr;
 
    # SSL parameters
    # ssl on;
