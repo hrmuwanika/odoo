338edbe04e74d173162251a270c64f88bd30fa8f
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-04-28 15:09:32 +0200

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -168,7 +168,7 @@ sudo chmod 640 /etc/${OE_CONFIG}.conf
 #--------------------------------------------------
 
 echo -e "\n========== Create Odoo systemd file ==============="
-cat <<EOF > /lib/systemd/system/odoo.service
+cat <<EOF > /lib/systemd/system/$OE_CONFIG.service
 
 [Unit]
 Description=Odoo Open Source ERP and CRM
@@ -178,7 +178,7 @@ After=network.target postgresql.service
 [Service]
 Type=simple
 PermissionsStartOnly=true
-SyslogIdentifier=odoo
+SyslogIdentifier=$OE_CONFIG
 User=$OE_USER
 Group=$OE_USER
 ExecStart=$OE_HOME_EXT/odoo-bin --config /etc/${OE_CONFIG}.conf  --logfile /var/log/${OE_USER}/${OE_CONFIG}.log
@@ -304,7 +304,7 @@ else
 fi
 
 echo -e "\n================== Starting Odoo Service ============================="
-sudo su root -c "/etc/init.d/$OE_CONFIG start"
+sudo systemctl start $OE_CONFIG"
 echo "\n========================================================================="
 echo "Done! The Odoo server is up and running. Specifications:"
 echo "Port: $OE_PORT"
