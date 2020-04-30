b625cda2ab5884fc4485aae09a8e5f56f9e5aaba
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-04-30 18:45:50 +0200

    Update odoo_install_debian.sh

--- odoo_install_debian.sh
+++ odoo_install_debian.sh
@@ -222,9 +222,6 @@ upstream odoochat {
 server {
     listen 80;
     listen [::]:80;
-    
-    # listen 443 ssl;
-    # listen [::]:443 ssl;
     server_name $WEBSITE_NAME;
    
     # Proxy settings
@@ -237,16 +234,6 @@ server {
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Proto \$scheme;
     proxy_set_header X-Real-IP \$remote_addr;
-
-    # SSL parameters
-    # ssl on;
-    # ssl_certificate /etc/letsencrypt/live/$WEBSITE_NAME/fullchain.pem;
-    # ssl_certificate_key /etc/letsencrypt/live/$WEBSITE_NAME/privkey.pem;
-    # ssl_trusted_certificate /etc/letsencrypt/live/$WEBSITE_NAME/chain.pem;
-    # ssl_session_timeout 30m;
-    # ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-    # ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
-    # ssl_prefer_server_ciphers on;
 
     # log
     access_log /var/log/nginx/$OE_USER-access.log;
