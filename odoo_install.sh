cf93240ba2798982faf62db7ed72edc347395ad2
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-05-08 18:32:09 +0200

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -90,9 +90,6 @@ sudo npm install -g rtlcss less less-plugin-clean-css
 ## https://www.odoo.com/documentation/13.0/setup/install.html#debian-ubuntu
 
 sudo apt install software-properties-common -y
-sudo apt-add-repository -y "deb http://security.ubuntu.com/ubuntu bionic-security main" 
-sudo apt update -y
-sudo apt install libxrender1 libfontconfig1 libx11-dev libjpeg62 libxtst6 fontconfig xfonts-75dpi xfonts-base libpng12-0 -y
 wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb
 sudo dpkg -i wkhtmltox_0.12.5-1.bionic_amd64.deb
 sudo apt -f install
