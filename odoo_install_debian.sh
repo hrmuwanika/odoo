77cda2e2ef9d818d8af017222b4f785862c5b2fe
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-07-06 17:25:11 +0200

    Update odoo_install_debian.sh

--- odoo_install_debian.sh
+++ odoo_install_debian.sh
@@ -42,6 +42,14 @@ ENABLE_SSL="True"
 ADMIN_EMAIL="odoo@example.com"
 ##
 
+#----------------------------------------------------
+# Disable password authentication
+#----------------------------------------------------
+sudo sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/UsePAM yes/UsePAM no/' /etc/ssh/sshd_config 
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo service sshd restart
+
 #--------------------------------------------------
 # Update Server
 #--------------------------------------------------
