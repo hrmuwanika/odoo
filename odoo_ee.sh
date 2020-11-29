48673514e11651f83b47c1e1c641c87e2f94ef63
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-11-29 17:13:00 +0200

    Create odoo_ee.sh

---
+++ odoo_ee.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#### upgrade odoo community to enterprise edition ####
+
+# Odoo 11: https://www.soladrive.com/downloads/enterprise-11.0.tar.gz
+# Odoo 12: https://www.soladrive.com/downloads/enterprise-12.0.tar.gz
+# Odoo 13: https://www.soladrive.com/downloads/enterprise-13.0.tar.gz
+# Odoo 14: https://www.soladrive.com/downloads/enterprise-14.0.tar.gz
+
+ systemctl stop odoo
+
+ mkdir /odoo/enterprise
+ mkdir /odoo/enterprise/addons
+ wget https://www.soladrive.com/downloads/enterprise-14.0.tar.gz
+ tar -zxvf enterprise-14.0.tar.gz
+ cp -rf enterprise-14.0/* /odoo/enterprise/addons
+
+# vim /etc/odoo-server.conf
+#    addons_path = /odoo/enterprise/addons,/odoo/odoo-server/addons
+
+ systemctl start odoo
