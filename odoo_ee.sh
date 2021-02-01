bf26ed8135df85aebfb3867fcfb076c0945c0c2e
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-02-01 19:23:34 +0200

    Update odoo_ee.sh

--- odoo_ee.sh
+++ odoo_ee.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 #### upgrade odoo community to enterprise edition ####
-
-# Odoo 11: https://www.soladrive.com/downloads/enterprise-11.0.tar.gz
-# Odoo 12: https://www.soladrive.com/downloads/enterprise-12.0.tar.gz
 # Odoo 13: https://www.soladrive.com/downloads/enterprise-13.0.tar.gz
 # Odoo 14: https://www.soladrive.com/downloads/enterprise-14.0.tar.gz
+cd /usr/src
+systemctl stop odoo
 
- systemctl stop odoo
-
- mkdir /odoo/enterprise
- mkdir /odoo/enterprise/addons
- wget https://www.soladrive.com/downloads/enterprise-14.0.tar.gz
- tar -zxvf enterprise-14.0.tar.gz
- cp -rf 14.0/* /odoo/enterprise/addons
-
+mkdir /odoo/enterprise
+mkdir /odoo/enterprise/addons
+wget https://www.soladrive.com/downloads/enterprise-14.0.tar.gz
+tar -zxvf enterprise-14.0.tar.gz
+cp -rf 14.0/* /odoo/enterprise/addons
+rm enterprise-14.0.tar.gz
 # vim /etc/odoo-server.conf
 #    addons_path = /odoo/enterprise/addons,/odoo/odoo-server/addons
 
- systemctl start odoo
+systemctl start odoo
