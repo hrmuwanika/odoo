388a6ea6a41ee982dd8b7d7d011852f32c2892d4
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-02-05 22:47:13 +0200

    Update install_odoo_debian.sh

--- install_odoo_debian.sh
+++ install_odoo_debian.sh
@@ -66,9 +66,16 @@ sudo echo "set mouse-=a" >> ~/.vimrc
 #--------------------------------------------------
 # Install PostgreSQL Server
 #--------------------------------------------------
-echo -e "\n================ Install PostgreSQL Server =========================="
-sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list > /dev/null
-sudo apt update
+# Create the file repository configuration:
+echo -e "\n=========== Installing PostgreSQL ================="
+sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list > /dev/null
+
+# Import the repository signing key:
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+# Update the package lists:
+sudo apt-get update
+
 sudo apt install -y postgresql postgresql-contrib
 sudo systemctl enable postgresql
 
