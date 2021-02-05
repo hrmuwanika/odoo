040a060767a99d9eeca363b57bd9f7a435cbe65f
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-02-05 22:00:00 +0200

    Update install_odoo_ubuntu.sh

--- install_odoo_ubuntu.sh
+++ install_odoo_ubuntu.sh
@@ -64,9 +64,6 @@ sudo apt autoremove -y
 #--------------------------------------------------
 echo -e "\n================ Install PostgreSQL Server =========================="
 sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list > /dev/null
-
-sudo apt-key add ACCC4CF8.asc
-
 sudo apt update
 sudo apt install -y postgresql postgresql-contrib
 sudo systemctl enable postgresql
