53a64b7744a2996f29f110bc068cca4d3d47f290
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-03-09 20:32:36 +0200

    Update install_odoo_debian.sh

--- install_odoo_debian.sh
+++ install_odoo_debian.sh
@@ -232,16 +232,13 @@ if [ $INSTALL_NGINX = "True" ]; then
   sudo systemctl start nginx
   
   cat <<EOF > /etc/nginx/sites-available/odoo
-  
- #odoo server
+#odoo server
 upstream odoo {
     server 127.0.0.1:$OE_PORT;
 }
-
 upstream odoochat {
     server 127.0.0.1:$LONGPOLLING_PORT;
 }
-
 # http to https redirection
 server {
     listen 80;
@@ -254,28 +251,24 @@ server {
     proxy_send_timeout 900s;
    
     # Add Headers for odoo proxy mode
-    proxy_set_header X-Forwarded-Host \$host;
-    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    proxy_set_header X-Forwarded-Proto \$scheme;
-    proxy_set_header X-Real-IP \$remote_addr;
-    
-    add_header X-Frame-Options "SAMEORIGIN";
-    add_header X-XSS-Protection "1; mode=block";
-    proxy_set_header X-Client-IP \$remote_addr;
-    proxy_set_header HTTP_X_FORWARDED_HOST \$remote_addr;
+    proxy_set_header X-Forwarded-Host $host;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_set_header X-Real-IP $remote_addr;
    
     # log
     access_log /var/log/nginx/$OE_USER-access.log;
     error_log /var/log/nginx/$OE_USER-error.log;
    
-    # Request for root domain
+    # Redirect requests to odoo backend server
     location / {
-    proxy_redirect off;
-    proxy_pass http://odoo;
+                 proxy_redirect off;
+                 proxy_pass http://odoo;
     }
    
+    # Redirect longpoll requests to odoo longpolling port
     location /longpolling {
-    proxy_pass http://odoochat;
+                 proxy_pass http://odoochat;
     }
    
     # Cache static files.
@@ -286,16 +279,12 @@ server {
                 proxy_pass http://odoo;
     }
    
-    #   enable data compression
+    # common gzip
+    gzip_types text/css text/scss text/plain text/xml application/xml application/json application/javascript;
     gzip on;
-    gzip_min_length 1100;
-    gzip_buffers    4   32k;
-    gzip_types  text/css text/less text/plain text/xml application/xml application/json application/javascript application/pdf image/jpeg image/png;
-    gzip_vary on;
-    client_header_buffer_size 4k;
-    large_client_header_buffers 4 64k;
     client_max_body_size 0;
 }
+
 EOF
 
   sudo mv ~/odoo /etc/nginx/sites-available/
