ab62e5b85859204714bebfd9f24cb870a6d03eba
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-06-13 15:33:24 +0200

    Update install_odoo_debian.sh

--- install_odoo_debian.sh
+++ install_odoo_debian.sh
@@ -62,7 +62,7 @@ sudo apt upgrade -y
 # UFW Firewall
 #--------------------------------------------------
 sudo apt install -y ufw 
-sudo ufw enable
+sudo ufw enable -y
 sudo ufw allow 22/tcp
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
