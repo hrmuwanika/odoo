f2332cabfcd3923016ee7acd596ddb011b7bbac5
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-06-13 16:30:36 +0200

    Update install_odoo_ubuntu.sh

--- install_odoo_ubuntu.sh
+++ install_odoo_ubuntu.sh
@@ -271,20 +271,27 @@ server {
     listen 80;
     server_name $WEBSITE_NAME;
    
-    # Proxy settings
-    proxy_read_timeout 720s;
-    proxy_connect_timeout 720s;
-    proxy_send_timeout 720s;
-   
     # Add Headers for odoo proxy mode
-    proxy_set_header X-Forwarded-Host $host;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header X-Forwarded-Proto $scheme;
-    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-Host \$host;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Real-IP \$remote_addr;
+    add_header X-Frame-Options "SAMEORIGIN";
+    add_header X-XSS-Protection "1; mode=block";
+    proxy_set_header X-Client-IP \$remote_addr;
+    proxy_set_header HTTP_X_FORWARDED_HOST \$remote_addr;
    
     # log
     access_log /var/log/nginx/$OE_USER-access.log;
     error_log /var/log/nginx/$OE_USER-error.log;
+    
+    # increase proxy buffer size
+    proxy_buffers   16  64k;
+    proxy_buffer_size   128k;
+    
+    proxy_read_timeout 900s;
+    proxy_connect_timeout 900s;
+    proxy_send_timeout 900s;
     
     # Redirect longpoll requests to odoo longpolling port
       location /longpolling {
