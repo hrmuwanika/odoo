0a4cae986a5d9f019081f16214bba19cb5345190
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-07-07 23:15:51 +0200

    Update install_odoo_debian.sh

--- install_odoo_debian.sh
+++ install_odoo_debian.sh
@@ -91,7 +91,7 @@ sudo apt -y install gnupg gnupg2
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ buster-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y postgresql-13 postgresql-client-13
+sudo apt install -y postgresql postgresql-client
 sudo systemctl start postgresql && sudo systemctl enable postgresql
 
 echo -e "\n=========== Creating the ODOO PostgreSQL User ================="
