3fe804bb6b3108d06b5b8132d82678d6180a8b4e
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-07-17 11:51:24 +0200

    Update install_odoo_debian.sh

--- install_odoo_debian.sh
+++ install_odoo_debian.sh
@@ -223,17 +223,12 @@ cat <<EOF > /lib/systemd/system/odoo.service
 Description=Odoo Open Source ERP and CRM
 Requires=postgresql.service
 After=network.target postgresql.service
-
 [Service]
 Type=simple
-PermissionsStartOnly=true
-SyslogIdentifier=odoo-server
 User=$OE_USER
 Group=$OE_USER
 ExecStart=$OE_HOME_EXT/odoo-bin --config /etc/${OE_CONFIG}.conf  --logfile /var/log/${OE_USER}/${OE_CONFIG}.log
 KillMode=mixed
-StandardOutput=journal+console
-
 [Install]
 WantedBy=multi-user.target
 
