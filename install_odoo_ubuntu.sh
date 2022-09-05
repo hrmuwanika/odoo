68f01cf410ebfbf0a4c40656ee20b8e74380e0c7
Henry Robert Muwanika <hrmuwanika@gmail.com>
2022-09-05 04:32:25 +0200

    Update install_odoo_ubuntu.sh

--- install_odoo_ubuntu.sh
+++ install_odoo_ubuntu.sh
@@ -50,10 +50,10 @@ ADMIN_EMAIL="hrmuwanika@gmail.com"
 #----------------------------------------------------
 # Disable password authentication
 #----------------------------------------------------
-sudo sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
-sudo sed -i 's/UsePAM yes/UsePAM no/' /etc/ssh/sshd_config 
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+# sudo sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+# sudo sed -i 's/UsePAM yes/UsePAM no/' /etc/ssh/sshd_config 
+# sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+# sudo systemctl restart sshd
 
 ##
 #--------------------------------------------------
